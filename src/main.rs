bfeee47f427cf942a6cf598c98abc0323736d9b9
Punyawat Rujipiranunt <rew150@outlook.com>
2022-02-23 10:52:29 +0700

    simple echo server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::{io::{self, Read, Write}, net::{TcpStream, TcpListener}, thread, time::Duration};
+
+fn handle_client(mut stream: TcpStream) {
+    println!("start");
+    let mut buf = [0u8; 128];
+    stream.set_read_timeout(None).unwrap();
+    loop {
+        let read = stream.read(&mut buf).unwrap();
+        if read == 0 {
+            break;
+        }
+        stream.write(&buf[0..read]).unwrap();
+    }
+    println!("wait 3 sec");
+    thread::sleep(Duration::from_secs(3));
+    println!("finish");
+}
+
+fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8888")?;
+    println!("listening");
+    for stream in listener.incoming() {
+        handle_client(stream?);
+    }
+    Ok(())
 }
